9fc1eb6ca003db3175fffc3cb63ff599685d1db9
土田拓 <taku_tsuchida@tsuchitatakunoMacBook-Air.local>
2020-05-16 11:27:18 +0900

    create db

--- server/main.py
+++ server/main.py
@@ -1,7 +1,20 @@
 import responder
+from tortoise import Tortoise
 from routes.auth import auth_routes
 
 api = responder.API()
+
+@api.on_event("startup")
+async def start_db_connection():
+    await Tortoise.init(
+        db_url="mysql://root:password@db:3306/docker_db",
+        modules={"models": ["models.auth"]}
+    )
+
+@api.on_event("shutdown")
+async def close_db_connection():
+    await Tortoise.close_connections()
+
 auth_routes(api)
 
 if __name__ == '__main__':
---
+++ server/migration.py
@@ -0,0 +1,12 @@
+from tortoise import Tortoise, run_async
+
+async def migrate():
+    await Tortoise.init(
+        db_url="mysql://root:password@db:3306/docker_db",
+        modules={"models": ["models.auth"]}
+    )
+    await Tortoise.generate_schemas()
+
+    print("Success migration!!")
+
+run_async(migrate())
